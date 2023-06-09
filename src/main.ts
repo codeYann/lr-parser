6536ab68f7636b562b6363f89a772f098abeebe5
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-06-09 14:19:29 -0300

    Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import { Lexer } from "./lexer";
+import { readFileContent } from "./utils";
+import path from "path";
+
+(() => {
+	async function main() {
+		const file = "./code/code.txt";
+		const absFile = path.resolve(__dirname, file);
+		const code = await readFileContent(absFile);
+		const lexer = new Lexer(code);
+		const tokens = lexer.tokenize();
+		console.log(tokens);
+	}
+	main().catch((error) => console.error(error));
+})();
