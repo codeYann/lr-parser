2e08981c07d63f677c4bbd1afbe3ec3a02ba3993
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-06-11 10:36:26 -0300

    Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
 import { Lexer } from "./lexer";
 import { readFileContent } from "./utils";
-import { buildLR0Items, Production } from "./parser";
 import path from "path";
-
-/**
- * grammar:
- * S -> if E then C | C
- * E -> a
- * C -> b
- *
- */
 
 (() => {
 	async function main() {
@@ -17,18 +8,16 @@ import path from "path";
 		const absFile = path.resolve(__dirname, file);
 		const code = await readFileContent(absFile);
 		const lexer = new Lexer(code);
-		const tokens = lexer.tokenize();
-		console.log(tokens);
 
-		const grammar: Production[] = [
-			{ left: "S", right: ["if", "E", "then", "C"] },
-			{ left: "S", right: ["C"] },
-			{ left: "E", right: ["a"] },
-			{ left: "C", right: ["b"] },
-		];
+		try {
+			const tokens = lexer.tokenize();
+			tokens.forEach((token) => {
+				console.log(token);
+			});
 
-		const it = buildLR0Items(grammar);
-		console.log(it);
+		} catch (error) {
+			console.error(error);
+		}
 	}
 	main().catch((error) => console.error(error));
 })();
