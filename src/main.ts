4232d2b64663ad002dfebd1aacc39e7d31be18ac
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-06-14 13:34:03 -0300

    Removing IIFE and just running main function

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,23 @@ import { readFileContent } from "./utils";
 import { Parser } from "./parser";
 import path from "path";
 
-(() => {
-	async function main() {
-		const file = "./code/code.txt";
-		const absFile = path.resolve(__dirname, file);
-		const code = await readFileContent(absFile);
-		const lexer = new Lexer(code);
+async function main() {
+	const file = "./code/code.txt";
+	const absFile = path.resolve(__dirname, file);
+	const code = await readFileContent(absFile);
+	const lexer = new Lexer(code);
 
+	try {
+		const tokens = lexer.tokenize();
 		try {
-			const tokens = lexer.tokenize();
-			try {
-				const parser = new Parser(tokens);
-				parser.parser();
-			} catch (error) {
-				console.error(error);
-			}
+			const parser = new Parser(tokens);
+			parser.parser();
 		} catch (error) {
 			console.error(error);
 		}
+	} catch (error) {
+		console.error(error);
 	}
-	main().catch((error) => console.error(error));
-})();
+}
+
+main().catch((error) => console.error(error));
